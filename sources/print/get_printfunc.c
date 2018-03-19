9941df0e9307a0ef2068705efd5f33cc3f4f20cd
mo0k <julien.moucadel@gmail.com>
2018-03-19 15:29:43 +0100

    fixe option priority

--- sources/print/get_printfunc.c
+++ sources/print/get_printfunc.c
@@ -6,7 +6,7 @@
 /*   By: mo0k <mo0k@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 15:27:17 by mo0k              #+#    #+#             */
-/*   Updated: 2018/03/18 21:30:20 by mo0k             ###   ########.fr       */
+/*   Updated: 2018/03/19 15:29:07 by mo0k             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 t_pfunc						get_printfunc(uint32_t options)
 {
-	if (GET_BIT(options, OPT_X)) //letter x
-		return (&print_option_x);
-	else if (GET_BIT(options, OPT_J))
+	if (GET_BIT(options, OPT_J))
 		return (&print_option_j);
+	else if (GET_BIT(options, OPT_X))
+		return (&print_option_x);
 	else if (GET_BIT(options, OPT_M))
 		return (&print_option_m);
 	else
-		//default a changer
 		return (&print_option_none);
 }
