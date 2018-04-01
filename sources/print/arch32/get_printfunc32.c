489f2503c224f4455c7ef6ee8e54ec95ae97e34b
mo0k <julien.moucadel@gmail.com>
2018-04-01 19:40:01 +0200

    ajust option u

--- sources/print/arch32/get_printfunc32.c
+++ sources/print/arch32/get_printfunc32.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_printfunc.c                                    :+:      :+:    :+:   */
+/*   get_printfunc32.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mo0k <mo0k@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 15:27:17 by mo0k              #+#    #+#             */
-/*   Updated: 2018/03/22 22:36:21 by mo0k             ###   ########.fr       */
+/*   Updated: 2018/04/01 19:38:55 by mo0k             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_pfunc						get_printfunc_arch32(uint32_t options)
 {
-	if (GET_BIT(options, OPT_J))
+	if (GET_BIT(options, OPT_J) || (GET_BIT(options, OPT_u) && !GET_BIT(options, OPT_M)))
 		return (&print_option_j_arch32);
 	else if (GET_BIT(options, OPT_X))
 		return (&print_option_x_arch32);
--- sources/print/arch64/get_printfunc64.c
+++ sources/print/arch64/get_printfunc64.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_printfunc.c                                    :+:      :+:    :+:   */
+/*   get_printfunc64.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mo0k <mo0k@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 15:27:17 by mo0k              #+#    #+#             */
-/*   Updated: 2018/03/22 21:28:37 by mo0k             ###   ########.fr       */
+/*   Updated: 2018/04/01 19:39:08 by mo0k             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_pfunc						get_printfunc_arch64(uint32_t options)
 {
-	if (GET_BIT(options, OPT_J))
+	if (GET_BIT(options, OPT_J) || (GET_BIT(options, OPT_u) && !GET_BIT(options, OPT_M)))
 		return (&print_option_j_arch64);
 	else if (GET_BIT(options, OPT_X))
 		return (&print_option_x_arch64);
