210fd5f7d9650dd3d982703e0f49674a35846f22
mo0k <julien.moucadel@gmail.com>
2018-07-25 03:31:22 +0200

    add strerror

--- lib/libopenfile/includes/open_file.h
+++ lib/libopenfile/includes/open_file.h
@@ -6,7 +6,7 @@
 /*   By: mo0k <mo0k@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 06:51:43 by mo0k              #+#    #+#             */
-/*   Updated: 2018/05/09 13:56:45 by mo0k             ###   ########.fr       */
+/*   Updated: 2018/07/24 19:56:25 by mo0k             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ enum e_return
 	ERROR_ARGS,
 	ERROR_OPEN,
 	ERROR_FSTAT,
+	ERROR_ISDIR,
 	ERROR_MMAP
 };
 
 int 	open_file(void **ptr, size_t *ptr_size, char *filepath, int flag_open);
+void	open_error(unsigned int error, char *pathfile);
 
 #endif
--- lib/libopenfile/sources/open_file.c
+++ lib/libopenfile/sources/open_file.c
@@ -6,35 +6,47 @@
 /*   By: mo0k <mo0k@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 11:24:02 by mo0k              #+#    #+#             */
-/*   Updated: 2018/05/09 13:54:37 by mo0k             ###   ########.fr       */
+/*   Updated: 2018/07/24 19:56:05 by mo0k             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/open_file.h"
+
+static char	**get_strerror()
+{
+	static char	*error[] = 
+	{
+		"Arguments no valid",
+		"Error open",
+		"Error fstat",
+		"Is a directory",
+		"Error mmap"
+	};
+	return (error);
+}
 
 int 	open_file(void **ptr, size_t *ptr_size, char *filepath, int flag_open)
 {
 	int				fd;
 	struct stat		buff;
 
-	//P_DEBUG_VARGS("filepath:%s\n", filepath);
 	if ((fd = open(filepath, flag_open)) < 0)
-	{
-		//perror(ERROR_STR);
 		return (ERROR_OPEN);	
-	}
 	if (fstat(fd, &buff) == -1)
-	{
-		//perror(ERROR_STR);
 		return (ERROR_FSTAT);
-	}
-	//check si dossier
+	if (buff.st_mode & S_IFDIR)
+		return (ERROR_ISDIR);
 	*ptr_size = buff.st_size;
 	if ((*ptr = mmap(0, *ptr_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
-	{
-		//perror(ERROR_STR);
 		return (ERROR_MMAP);
-	}
-	//P_DEBUG("load file OK\n");
 	return (0);
+}
+
+void		open_error(unsigned int error, char *pathfile)
+{
+	if (error > 4)
+		return ;
+	else
+		ft_dprintf(2, "ft_nm: %s %s\n", pathfile, get_strerror()[error]);
+	return ;
 }
